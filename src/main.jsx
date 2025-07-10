84f006b9293993e14d7e152e6f229276a9dc6f82
SKhanam-tech <skhanam17284@gmail.com>
2025-07-11 02:19:30 +0530

    add toast container

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
